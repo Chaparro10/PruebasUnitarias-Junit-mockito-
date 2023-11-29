d2b5b51faf5aefe2ff9c1c1ef046314495eaab0e
Chaparro10 <19170614@itculiacan.edu.mx>
2023-11-29 11:49:28 -0700

    Se agregaron mas Test de Repository

--- pruebas-unitarias/pruebas-unitarias/src/test/java/com/apirest/pruebasunitarias/Repository/EmpleadoRepositorytest.java
+++ pruebas-unitarias/pruebas-unitarias/src/test/java/com/apirest/pruebasunitarias/Repository/EmpleadoRepositorytest.java
@@ -1,6 +1,6 @@
 package com.apirest.pruebasunitarias.Repository;
 
-import  static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.apirest.pruebasunitarias.models.Empleado;
 import com.apirest.pruebasunitarias.repository.IEmpleadoRepository;
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest
 public class EmpleadoRepositorytest {
     @Autowired
-    private IEmpleadoRepository  empleadoRepository;
+    private IEmpleadoRepository empleadoRepository;
 
 
     @Test
@@ -30,6 +33,58 @@ public class EmpleadoRepositorytest {
         // then - verificar la salida
         assertThat(empleadoGuardado).isNotNull();
         assertThat(empleadoGuardado.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void testListarEmpleado() {
+
+        List<Empleado> listEmpleado = empleadoRepository.findAll();
+        System.out.println("Epleados:" + listEmpleado);
+        //then
+        assertThat(listEmpleado).isNotNull();
+        assertThat(listEmpleado.size()).isEqualTo(5);
+    }
+
+    @Test
+    void testListaEmpleadoXId() {
+        Long idEmpleadoExistente = 1L;
+
+        Optional<Empleado> empleadoOptional = empleadoRepository.findById(idEmpleadoExistente);
+        System.out.println("Empleado id=" + empleadoOptional);
+        assertThat(empleadoOptional).isNotNull();
+    }
+
+    @Test
+    void testActualizarEmpleado() {
+        Long idEmpleadoExistente = 5L;
+
+        Optional<Empleado> empleadoOptional = empleadoRepository.findById(idEmpleadoExistente);
+
+        // Verificar que el Optional no sea vacío y extraer el empleado si está presente
+        assertThat(empleadoOptional).isPresent();
+        Empleado empleado = empleadoOptional.get();
+
+        System.out.println("Empleado ha actualizar" + empleadoOptional);
+
+        // Realizar la actualización del empleado
+        empleado.setNombre("NuevoNombre");
+        empleado.setApellido("NuevoApellido");
+        empleado.setEmail("nuevo@email.com");
+
+        // Guardar el empleado actualizado en la base de datos
+        Empleado empleadoActualizado = empleadoRepository.save(empleado);
+
+        // Verificar que el empleado actualizado no sea nulo
+        assertThat(empleadoActualizado).isNotNull();
+
+        // Verificar que los datos del empleado se hayan actualizado correctamente
+        assertThat(empleadoActualizado.getNombre()).isEqualTo("NuevoNombre");
+        assertThat(empleadoActualizado.getApellido()).isEqualTo("NuevoApellido");
+        assertThat(empleadoActualizado.getEmail()).isEqualTo("nuevo@email.com");
+
+
+        System.out.println("Empleado actualizado" + empleadoActualizado);
+
     }
 
 }
---
+++ pruebas-unitarias/pruebas-unitarias/src/test/java/com/apirest/pruebasunitarias/Service/EmpleadoServiceTest.java
@@ -0,0 +1,4 @@
+package com.apirest.pruebasunitarias.Service;
+
+public class EmpleadoServiceTest {
+}
